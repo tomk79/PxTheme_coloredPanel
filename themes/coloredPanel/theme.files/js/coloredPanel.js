5ec6c73a47e198918eee89c285e7e31fa8b900b8
Tomoya Koyanagi <tomk79@gmail.com>
2013-09-21 22:32:54 +0900

    背景色の切り替わり演出を少し自然な感じにした。
    - 画面全体のフェードイン・アウトに合わせた。

--- themes/coloredPanel/theme.files/js/coloredPanel.js
+++ themes/coloredPanel/theme.files/js/coloredPanel.js
@@ -33,15 +33,16 @@
 				// console.log('complete');
 				status = 3;
 				window.scroll(0,0);
+
+				var hue = Math.floor( Math.random() * 100 );
+				var hue = convertString2ASCII( document.title )/7313666355%100;
+				// console.log(hue);
+				var rgb = $.hsb2rgb(hue,60,100);
+				// console.log('rgb('+Math.round(rgb['r'])+','+Math.round(rgb['g'])+','+Math.round(rgb['b'])+')');
+				$('body').css({'background-color':'rgb('+Math.round(rgb['r'])+','+Math.round(rgb['g'])+','+Math.round(rgb['b'])+')'});
+
 				$('.theme_outline').fadeIn('slow', function(){
 					status = 0;
-
-					var hue = Math.floor( Math.random() * 100 );
-					var hue = convertString2ASCII( document.title )/7313666355%100;
-					// console.log(hue);
-					var rgb = $.hsb2rgb(hue,60,100);
-					// console.log('rgb('+Math.round(rgb['r'])+','+Math.round(rgb['g'])+','+Math.round(rgb['b'])+')');
-					$('body').css({'background-color':'rgb('+Math.round(rgb['r'])+','+Math.round(rgb['g'])+','+Math.round(rgb['b'])+')'});
 				});
 			} ,
 			'target':[
